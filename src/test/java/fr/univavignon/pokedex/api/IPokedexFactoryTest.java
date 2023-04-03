d011179c273a0ff36b45991b074db00bcd0bdbc1
sebastienguimety <seb.guimety@gmail.com>
2023-04-03 15:49:30 +0200

    All test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,43 @@
+package fr.univavignon.pokedex.api;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+
+public class IPokedexFactoryTest extends TestCase {
+
+    IPokedexFactory pokedexFactory;
+    PokemonMetadata aquali;
+    PokemonMetadata bulbizarre;
+
+    @Before
+    public void init() {
+        pokedexFactory = Mockito.mock(IPokedexFactory.class);
+        bulbizarre = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        aquali = new PokemonMetadata(133, "Aquali", 186, 186, 260);
+
+    }
+
+    @Test
+    public void testCreatePokedex() {
+        IPokemonMetadataProvider pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        IPokemonFactory pokemonFactory = Mockito.mock(IPokemonFactory.class);
+        Mockito.doReturn(Mockito.mock(IPokedex.class)).when(pokedexFactory)
+                .createPokedex(Mockito.any(pokemonMetadataProvider.getClass()), Mockito.any(pokemonFactory.getClass()));
+        Assert.assertNotNull(pokedexFactory.createPokedex(Mockito.mock(IPokemonMetadataProvider.class),
+                Mockito.mock(IPokemonFactory.class)));
+        Assert.assertEquals(Mockito.mock(IPokedex.class).getClass(), pokedexFactory
+                .createPokedex(Mockito.mock(IPokemonMetadataProvider.class), Mockito.mock(IPokemonFactory.class))
+                .getClass());
+
+    }
+
+}
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,35 +1,39 @@
 package fr.univavignon.pokedex.api;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest extends TestCase {
 
+    IPokemonMetadataProvider pokemonMetadataProvider;
+
+    PokemonMetadata pok1;
+    PokemonMetadata pok2;
+
+    @Before
+    public void init() {
+        pokemonMetadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        pok1 = new PokemonMetadata(1, "Bulbizarre", 126, 126, 90);
+        pok2 = new PokemonMetadata(2, "Aquali", 186, 186, 260);
+    }
+
+    @Test
     public void testGetPokemonMetadata() throws PokedexException {
 
-        // Créer un mock de IPokemonMetadataProvider
-        IPokemonMetadataProvider provider = Mockito.mock(IPokemonMetadataProvider.class);
+        Mockito.doReturn(pok2).when(pokemonMetadataProvider).getPokemonMetadata(2);
+        Mockito.doReturn(pok1).when(pokemonMetadataProvider).getPokemonMetadata(1);
+        assertEquals(pok1, pokemonMetadataProvider.getPokemonMetadata(1));
+        assertEquals(pok2, pokemonMetadataProvider.getPokemonMetadata(2));
 
-        // Créer une instance de la classe PokemonMetadata avec des valeurs appropriées
-        PokemonMetadata metadata = new PokemonMetadata(25, "Pikachu", 112, 96, 111);
-
-        // Définir le comportement du mock pour qu'il retourne l'instance de PokemonMetadata créée ci-dessus
-        when(provider.getPokemonMetadata(25)).thenReturn(metadata);
-
-        // Appeler la méthode getPokemonMetadata du mock de IPokemonMetadataProvider
-        PokemonMetadata result = provider.getPokemonMetadata(25);
-
-        // Vérifier que la méthode a bien été appelée avec la valeur de l'index correspondant
-        verify(provider).getPokemonMetadata(25);
-
-        // Vérifier que l'instance de PokemonMetadata retournée a les valeurs appropriées
-        assertEquals(25, result.getIndex());
-        assertEquals("Pikachu", result.getName());
-        assertEquals(112, result.getAttack());
-        assertEquals(96, result.getDefense());
-        assertEquals(111, result.getStamina());
+        Mockito.doThrow(new PokedexException("L'index fourni n'existe pas ")).when(pokemonMetadataProvider)
+                .getPokemonMetadata(Mockito.intThat(i -> i < 0 || i > 150));
+        assertThrows(PokedexException.class, () -> pokemonMetadataProvider.getPokemonMetadata(-189));
+        assertThrows(PokedexException.class, () -> pokemonMetadataProvider.getPokemonMetadata(313));
     }
 }
